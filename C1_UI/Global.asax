77cd0186a93da9c20c26ee4586061ed165863c7e
Santiago <nietto.santiago@gmail.com>
2025-11-14 21:36:14 -0300

    capas del proyecto

---
+++ C1_UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="C1_UI.Global" Language="C#" %>
---
+++ C4_ENTIDAD/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4_ENTIDAD
+{
+    public class Class1
+    {
+    }
+}
