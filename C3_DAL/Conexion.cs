7fbdfeae364701001c3153c26c92b2c7ed637cc6
Santiago <nietto.santiago@gmail.com>
2025-11-17 19:29:55 -0300

    featch: entidades cliente y domicilio creadas con su conexion

---
+++ C3_DAL/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C4_ENTIDAD;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace C3_DAL
+{
+    public class Conexion
+    {
+        public string connectionString = "data source=compu; initial catalog=anaserena_pms_db; integrated security=sspi";
+
+        public SqlConnection ObtenerConxeion()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+
+    }
+}
---
+++ C4_ENTIDAD/Cliente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4_ENTIDAD
+{
+    public class Cliente
+    {
+        private int _idCliente;
+        private string _nombre;
+        private string _apellido;
+        private string _telefono;
+        private string _email;
+        private bool _activo;
+        private DateTime _fechaIngreso;
+
+        public int IdCliente { get => _idCliente; set => _idCliente = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public string Telefono { get => _telefono; set => _telefono = value; }
+        public string Email { get => _email; set => _email = value; }
+        public bool Activo { get => _activo; set => _activo = value; }
+        public DateTime FechaIngreso { get => _fechaIngreso; set => _fechaIngreso = value; }
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(int idCliente, string nombre, string apellido, string telefono, string email, bool activo, DateTime fechaIngreso)
+        {
+            _idCliente = idCliente;
+            _nombre = nombre;
+            _apellido = apellido;
+            _telefono = telefono;
+            _email = email;
+            _activo = activo;
+            _fechaIngreso = fechaIngreso;
+        }
+
+        public string ObtenerNombreCompleto()
+        {
+            return $"{Nombre} {Apellido}";
+        }
+    }
+}
